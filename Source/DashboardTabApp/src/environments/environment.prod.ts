7843bdcfc18245a4e60381b01978b0ace39c7104
hendrilee <dutainfomedia@gmail.com>
2020-11-30 10:42:32 +0700

    Update environment.prod.ts

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://enovadeqbotnew.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://qbotenovade.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
         tenantId: "f3b0b74e-b987-4e66-928a-6f077de4c833",
-        clientId: "225741f1-3c92-44bf-994d-c22537df11de",
+        clientId: "2c212938-677f-4678-9cd2-55561e276aa9",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
